e2897c8578bdee037e2f12165755de0c9296bcaf
asimsharf <asimsharf@gmail.com>
2025-03-16 02:05:01 +0300

    up to date in 0.002s

--- src/main/java/com/sudagoarth/events/Event/Repositories/EntityTypeRepository.java
+++ src/main/java/com/sudagoarth/events/Event/Repositories/EntityTypeRepository.java
@@ -1,9 +1,10 @@
 package com.sudagoarth.events.Event.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.sudagoarth.events.Event.Entities.EntityType;
-
+@Repository
 public interface EntityTypeRepository extends JpaRepository<EntityType, Long> {
 
 }
